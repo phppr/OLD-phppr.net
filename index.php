d0c07e2684a3b109712fb4aa96bec05a8db81b24
Andre Cardoso <andrecardosodev@gmail.com>
2013-12-06 14:47:10 -0200

    Adicionada página estática para espera do site oficial

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
  *
  * @var bool
  */
-define('WP_USE_THEMES', true);
-
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+//define('WP_USE_THEMES', true);
+//
+///** Loads the WordPress Environment and Template */
+//require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+require_once './start.php';
---
+++ start.php
@@ -0,0 +1,8 @@
+<div class="image" style="text-align: center;">
+	<h1>PHP PR</h1>
+	<img src="phppr.png" style="width: 800px;">
+	<br />
+	<a href="https://www.facebook.com/groups/1398320767076609/">
+		Acompanhe a gente no Facebook
+	</a>
+</div>
