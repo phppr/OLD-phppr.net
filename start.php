809ca1ec7ef102a9c2c854b423acdca74759d5c0
Andre Cardoso <andrecardosodev@gmail.com>
2013-12-14 19:18:51 -0200

    Correção do código de track do Google Analytics
    O track de eventos foi alterado da página temporária para o header do tema
    twenty-fourteen (2014)

--- start.php
+++ start.php
@@ -3,19 +3,6 @@
 		<title>PHP PR - Comunidade de desenvolvedores PHP do estado do Paraná</title>
 		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 		
-		<!-- Google Analytics -->
-		<script>
-		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-		  ga('create', 'UA-46283701-1', 'phppr.net');
-		  ga('send', 'pageview');
-
-		</script>
-		<!-- Google Analytics -->
-		
 	</head>
 	<body>
 		<div class="image" style="text-align: center;">
